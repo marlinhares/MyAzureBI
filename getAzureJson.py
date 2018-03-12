b32d94f309f52aa224be1386bef7c8d1f2361548
Marcelo Linhares <marcelo.linhares@planejamento.gov.br>
2018-03-12 16:16:46 -0300

    Evolutions on json getter

--- getAzureJson.py
+++ getAzureJson.py
@@ -1,127 +1,114 @@
-import utils.*
+import utils
 import json
 import os
-from elasticsearch import Elasticsearch
 from datetime import date
 
 
+def getFlavour(region):
+    a = os.popen('az vm list-sizes -l ' + region).read()
+    return json.loads(a)
 
 
-def getDisks():
-    return
-    
+def getListSizes():
+    r = getFlavour('BRAZILSOUTH') + getFlavour('EASTUS') + getFlavour('WESTUS')
 
-def getVms():
-    return
-    
-def getStorageAccs():
-    return
-    
-def getBlobs:
-    return
-    
-def getContainers():
-    return
-    
+    return r
 
 
-
-# busca data atual
-d = date.today()
-d = "2018-03-09"
-
-# json com os Accounts
-#a = os.popen('az account list --query "[?name==\'17-0134-B-GM-ASCOM-PORTAL-PLANEJAMENTO-MP-PRD\'].{id:id, name:name, state:state, tenantid:tenantid}"').read()
-a = os.popen('az account list --query "[].{id:id, name:name, state:state, tenantid:tenantid}"').read()
-subscriptions = json.loads(a)
-
-# json com os hardwareProfiles (flavours)
-a = os.popen('az vm list-sizes -l BRAZILSOUTH').read()
-listsizes = json.loads(a)
-
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-
-for s in subscriptions:
-    s['date'] = d
-    a = os.popen('az account set -s ' + s['id']).read()
-
-    # disks
+def getDisks(subscription, fdate):
     a = os.popen('az disk list').read()
     disks = json.loads(a)
 
     for disk in disks:
-    	print "Buscando disco " + disk['name'] + " da subsc: " + s['name']
-        # disk['subscription'] = s
-        disk['date'] = d
+        disk['subscription'] = subscription
+        disk['date'] = fdate
 
-        # es.index(index='disk', doc_type='disks', body=disk)
+    return disks
 
-    # storage
-    
+
+def getStorageAccs(subscription, fdate):
     a = os.popen('az storage account list').read()
     storageaccounts = json.loads(a)
 
     for sa in storageaccounts:
-        # sa['subscription'] = s
-        sa['date'] = d
+        sa['subscription'] = subscription
+        sa['date'] = fdate
 
-        # container
-        a = os.popen('az storage container list --account-name="' + sa['name'] + '"').read()
-        containers = json.loads(a)
-
-        for container in containers:
-            print "Buscando blobs do container: " + container['name']
-
-            a = os.popen('az storage blob list --container-name="' + container['name'] + '" --account-name="' + sa['name'] + '"').read()
-            blobs = json.loads(a)
-            # associate blobs to containers
-            container['blobs'] = blobs
-
-            # todo files
-
+        containers = getContainers(sa['name'])
         sa['containers'] = containers
-        
-        # storage files
-        a = os.popen('az storage share list --account-name="' + sa['name'] + '"').read()
-        try:
-            shares = json.loads(a)
-        except ValueError, e:
-        	shares = []
-        
 
-        s['diskshares'] = shares
+        shares = getFileShares(sa['name'])
+        sa['diskshares'] = shares
 
-        # es.index(index='storageaccount', doc_type='sas', body=sa)
-    
-    
-    # vms
-    # a = os.popen('az vm list --query "[].{RG:resourceGroup,Name:name,HardProfile:hardwareProfile.vmSize,storageProfile:storageProfile}"').read()
+    return storageaccounts
+
+
+def getContainers(sa):
+    a = os.popen('az storage container list --account-name="' + sa + '"').read()
+    containers = json.loads(a)
+
+    for container in containers:
+        blobs = getBlobs(ac, container['name'])
+        container['blobs'] = blobs
+
+    return containers
+
+
+def getBlobs(sa, container):
+    a = os.popen('az storage blob list --container-name="' + container + '" --account-name="' + sa + '"').read()
+    blobs = json.loads(a)
+
+    return blobs
+
+
+def getFileShares(sa):
+    a = os.popen('az storage share list --account-name="' + sa + '"').read()
+    try:
+        shares = json.loads(a)
+    except ValueError, e:
+        shares = []
+
+    return shares
+
+
+
+def getVms(subscription, fdate):
     a = os.popen('az vm list').read()
     vms = json.loads(a)
 
-    # adiciona subscription ao json de vms
     for vm in vms:
-    	print "buscando vm " + vm['name']
-        # vm['subscription'] = s
-        vm['date'] = d
-        print vm
-        vm['flavour'] = retFirstVal(listsizes, 'name', vm['hardwareProfile']['vmSize'])
+        vm['date'] = fdate
+        vm['flavour'] = utils.retFirstVal(listsizes, 'name', vm['hardwareProfile']['vmSize'])
+        vm['subscription'] = vm
 
-        es.index(index='vm', doc_type='vms', body=vm)
 
-    # associate disks in subscription
-    s['disks'] = disks
-    # associate storageaccounts in subscription
-    s['storageaccounts'] = storageaccounts
-    # associate vms in subscription
+
+# busca data atual
+fdate = date.today()
+fdate = "2018-03-12"
+
+# get accounts json
+#a = os.popen('az account list --query "[?name==\'17-0134-B-GM-ASCOM-PORTAL-PLANEJAMENTO-MP-PRD\'].{id:id, name:name, state:state, tenantid:tenantid}"').read()
+a = os.popen('az account list --query "[].{id:id, name:name, state:state, tenantid:tenantid}"').read()
+subscriptions = json.loads(a)
+
+
+# list with all flavours
+listsizes = getListSizes()
+
+
+for s in subscriptions:
+
+    a = os.popen('az account set -s ' + s['id']).read()
+
+    disks = getDisks(s, fdate)
+    storageaccounts = getStorageAccs(s, fdate)
+    vms = getVms(s, fdate)
+
     s['vms'] = vms
+    s['date'] = fdate
 
     # es.index(index='subscription', doc_type='subscri', body=s)
     with open('subs.json', 'w') as outfile:
         json.dump(subscriptions, outfile)
 
-    print s
-
-
-print "Json Novo:", vms
-print "Jesus Cristo"
